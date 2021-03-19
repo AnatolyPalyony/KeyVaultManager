7d0ba0096e3ed4dce15fe90b74d730b7fc168b64
anatoly.palyony <anatoly.palyony@globallogic.com>
2021-03-19 15:59:13 +0200

    some cleaning

--- KeyVaultManager.ps1
+++ KeyVaultManager.ps1
@@ -10,10 +10,8 @@ Param (
 
 Function ExportSecrets {
     Param(
-        [parameter(Mandatory = $true)]
-        [string] $KeyVaultName, 
-        [parameter(Mandatory = $true)]
-        [System.IO.FileInfo] $FileName
+        [parameter(Mandatory = $true)][string] $KeyVaultName, 
+        [parameter(Mandatory = $true)][System.IO.FileInfo] $FileName
     );
     $Fullpath = Resolve-Path -Path $FileName.Directory
     Write-Host "Exporting to $Fullpath"
@@ -22,7 +20,6 @@ Function ExportSecrets {
         Write-Host "Check your filepath and try again please!"
         exit
     }
-    Write-Host "Exporting to $FileName"
     $secrets = Get-AzKeyVaultSecret -VaultName $KeyVaultName 
     $keys = @{}
     foreach ($secret in $secrets) {
@@ -36,22 +33,17 @@ Function ExportSecrets {
             Write-Host "Has been disabled. skipping"
         }           
     }
-    # $keys.GetEnumerator() |
-    #   Select-Object -Property Key, Value | Export-Csv -NoTypeInformation -Path $FileName -Force
 
-     $jsonname = ([String]$FileName).Replace(".csv",".json")
+     $jsonname = $FileName
      $jsonobject = [PSCustomObject]$keys
      $jsonobject | ConvertTo-Json -Depth 5 | Out-File $jsonname
     
 }
 Function ImportSecrets {
     Param(
-        [parameter(Mandatory = $true)]
-        [string] $KeyVaultName, 
-        [parameter(Mandatory = $true)]
-        [System.IO.FileInfo] $FileName,
-        [parameter(Mandatory = $false)]
-        [string] $Description
+        [parameter(Mandatory = $true)][string] $KeyVaultName, 
+        [parameter(Mandatory = $true)][System.IO.FileInfo] $FileName,
+        [parameter(Mandatory = $false)][string] $Description
     );
     
     if (!(Test-Path -Path $FileName)) {
@@ -67,27 +59,17 @@ Function ImportSecrets {
             DisableSecrets -KeyVaultName $KeyVaultName
         }
 
-    
         Write-Host "Importing from $FileName"
         $Expires = (Get-Date).AddYears(2).ToUniversalTime()
         $NBF = (Get-Date).ToUniversalTime()
         $ContentType = 'txt'
         if ($Description.Length -gt 0) {
             Write-Host $Description
-            $Tags = @{ 'Priority' = 'medium'; 'Department' = 'true'; 'Source' = $FileName; 'Description' = $Description }
+            $Tags = @{ 'Priority' = 'medium'; 'Department' = 'GoodDepartment'; 'Source' = $FileName; 'Description' = $Description }
         }
         else {
-            $Tags = @{ 'Priority' = 'medium'; 'Department' = 'true'; 'Source' = $FileName; 'Description' = '' }    
+            $Tags = @{ 'Priority' = 'medium'; 'Department' = 'GoodDepartment'; 'Source' = $FileName; 'Description' = '' }    
         }
-
-        <#  Import-csv $FileName | ForEach-Object {
-            $tkey = $_.Key
-            $tval = $_.Value
-            $secret = ConvertTo-SecureString -string $tval  -asplaintext -force
-            Start-ThreadJob -ScriptBlock {
-                Set-AzKeyVaultSecret -vaultname $Using:KeyVaultName -name $Using:tkey -SecretValue $Using:secret -ContentType $Using:ContentType -Expires $Using:Expires -NotBefore $Using:NBF -Tags $Using:Tags
-            }
-        } #>
 
         $json = Get-Content $FileName | Out-String | ConvertFrom-Json -AsHashtable
  
@@ -105,42 +87,34 @@ Function ImportSecrets {
 }
 Function DisableSecrets {
     Param(
-        [parameter(Mandatory = $true)]
-        [string] $KeyVaultName
+        [parameter(Mandatory = $true)][string] $KeyVaultName
     );
     $secrets = Get-AzKeyVaultSecret -VaultName $KeyVaultName 
     foreach ($secret in $secrets) {
         Start-ThreadJob -ScriptBlock {
             $tsecret = $Using:secret
             $tsecretName = $tsecret.Name
-            
             if ( $tsecret.Enabled ) {
-                Write-Host "Disabling" $tsecretName
-                
+                Write-Host "Disabling" $tsecretName 
                 $keyvalue = ConvertTo-SecureString -String (Get-AzKeyVaultSecret -VaultName $Using:KeyVaultName -name $tsecretName -AsPlainText) -AsPlainText -Force
                 Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name $tsecretName -SecretValue $keyvalue -Disable
             }
             else {
-                 
                 Write-Warning $tsecretName 
                 Write-Host "Has been already disabled. skipping"
             }      
         }
     }
     Measure-Command -Expression { Get-Job | Wait-Job } 
-
 }
 Function ListSecrets {
     Param(
-        [parameter(Mandatory = $true)]
-        [string] $KeyVaultName
+        [parameter(Mandatory = $true)][string] $KeyVaultName
     );
-    
     $secrets = Get-AzKeyVaultSecret -VaultName $KeyVaultName 
     $keys = @{}
     foreach ($secret in $secrets) {
         $secretName = $secret.name
-       
         if ( $secret.Enabled ) {
             $keyvalue = (Get-AzKeyVaultSecret -VaultName $keyvaultName -name $secretName -AsPlainText)         
             $keys.Add("$secretName", "$keyvalue")
